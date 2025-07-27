79b581a55939141981dd059248119ffec0186384
Jyothi D <dayanandajyothi1997@gmail.com>
2025-07-27 14:27:07 +0000

    commit

---
+++ models/staging/pre_post_model.sql
@@ -0,0 +1,16 @@
+{{ config(
+    materialized="table",
+    pre_hook="delete from {{ this }} where USER_ID = null",
+    post_hook="TRUNCATE TABLE {{ref('orders')}}"
+) }}
+
+WITH tb1 as(
+    select 
+    * from {{ref('orders')}})
+select * from tb1
+
+
+
+
+
+
